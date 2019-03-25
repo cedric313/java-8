17075f2008954395808c76d9b02a38ca884456cc
cedric <casas.cedric@gmail.com>
2019-03-25 11:41:43 +0100

    fichier quete 8

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,33 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override 
+    public void takeOff() {
+        System.out.println("ok");
+    }
+
+    @Override 
+    public int ascend(int meters) {
+        System.out.println("Eagles fly at " + meters);
+        return meters;
+    }
+
+    
+    public int descend(int meters) {
+        System.out.println("Eagles descend at " + meters);
+        return meters;
+    }
+        
+    @Override 
+    public void land() {
+        System.out.println("Arizona");
+    }
+
+    @Override 
+    public void glide(){
+        System.out.println("Eagles don't glide");
+    }
+
+
 }
